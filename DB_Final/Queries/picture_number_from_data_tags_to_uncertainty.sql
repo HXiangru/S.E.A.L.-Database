880708b4cf940f5509c4552369fbcb41cc5a70ba
XiangRu Huang <122172357+hxr303@users.noreply.github.com>
2024-01-30 10:37:49 +0100

    Delete DB_Final directory

--- DB_Final/Queries/picture_number_from_data_tags_to_uncertainty.sql
+++
@@ -1,4 +0,0 @@
-UPDATE uncertainty
-SET picture_number = data_tags.picture_number
-FROM data_tags
-WHERE uncertainty.serial_id = data_tags.logic_tag;
--- DB_Final/Queries/store_image_as_binary_file.sql
+++
@@ -1,4 +0,0 @@
--- Update stored_images column in data_reference with image data from the file system
-UPDATE data_reference
-SET stored_image = pg_read_binary_file(link_path::text)
-WHERE link_path IS NOT NULL;
